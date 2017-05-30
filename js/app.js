eebd66525a7cf5417986e52315499eb4aca29602
amalakamala <amalakamala.a@gmail.com>
2017-05-30 13:17:15 -0400

    inicio js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ var lasImagenes = Array.from(document.getElementsByClassName("col-4"));
+
+ lasImagenes.forEach(function(e){
+ 	e.addEventListener("click", boton)
+ });
+
+
+ function boton (){
+ 	alert("hola")
+ }
